115927cfaf159348ea1cbca3f69ebd0e50d35090
NataBash <2864tasha@gmail.com>
2025-07-07 14:46:13 +0200

    Подключает useInitAuth ко всему приложению для доступа и сохранения имени и токенов юзера

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,13 +1,17 @@
+'use client'
+
 import Nav from '@/components/Nav/Nav';
 import SideBar from '@/components/SideBar/SideBar';
 import Bar from '@/components/Bar/Bar';
 import styles from './layout.module.css';
+import { useInitAuth } from '@/hooks/useInitAuth';
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  useInitAuth();
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
