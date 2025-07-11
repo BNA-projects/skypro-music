27b521856ce475c8a9f18856e8443b5f49d6ffb9
NataBash <2864tasha@gmail.com>
2025-07-11 17:20:06 +0200

    Добавляет на избранных треках информацию о авторизации

--- src/app/(dashboard)/my-tracks/page.tsx
+++ src/app/(dashboard)/my-tracks/page.tsx
@@ -1,33 +1,53 @@
 'use client';
 
 import CenterBlock from '@/components/CenterBlock/CenterBlock';
-import { useEffect, useCallback, useState } from 'react';
-
+import { useEffect } from 'react';
 import { fetchAllFavoriteTracks } from '@/services/tracks/tracksApi';
 import { handleAxiosError } from '@/utils/handleAxiosError';
-import { useAppSelector } from '@store/store';
+import { useAppDispatch, useAppSelector } from '@store/store';
 import { setFavoritePlayList } from '@store/features/trackSlice';
 
 export default function MyTracks() {
   const access = useAppSelector((state) => state.auth.access);
   const tracks = useAppSelector((state) => state.tracks.favoritePlayList);
-
-  const getAllFavoriteTracks = useCallback(async () => {
-    try {
-      const data = await fetchAllFavoriteTracks(access);
-      if (data) setFavoritePlayList(data);
-    } catch (error) {
-      handleAxiosError(error);
-    }
-  }, []);
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
-    getAllFavoriteTracks();
-  }, []);
+    if (!access) return;
 
-  return (
-    <>
-      <CenterBlock title="Мои треки" tracks={tracks} />
-    </>
-  );
+    const getAllFavoriteTracks = async () => {
+      try {
+        const data = await fetchAllFavoriteTracks(access);
+        if (data) dispatch(setFavoritePlayList(data));
+      } catch (error) {
+        handleAxiosError(error);
+      }
+    };
+
+    getAllFavoriteTracks();
+  }, [access, dispatch]);
+
+  if (!access) {
+    return (
+      <div
+      style={{
+        maxWidth: '400px',
+        margin: '50px auto',
+        padding: '20px',
+        border: '2px solid #e53e3e',
+        borderRadius: '8px',
+        backgroundColor: '#fff5f5',
+        color: '#c53030',
+        fontWeight: '600',
+        fontSize: '18px',
+        textAlign: 'center',
+        boxShadow: '0 0 10px rgba(229, 62, 62, 0.3)',
+      }}
+    >
+        Для просмотра плейлиста необходимо авторизоваться
+      </div>
+    );
+  }
+
+  return <CenterBlock title="Мои треки" tracks={tracks} />;
 }
