c14beba0bed94ef7010f80a549d617428a2c0a81
NataBash <2864tasha@gmail.com>
2025-07-13 13:01:05 +0200

    Получает все треки из состояния и обновляет состояние при новых данных с сервера

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -2,27 +2,28 @@
 
 import CenterBlock from '@/components/CenterBlock/CenterBlock';
 import { fetchAllTracks } from '@/services/tracks/tracksApi';
-import { Track } from '@/sharesTypes/sharesTypes';
 
-import { useEffect, useCallback, useState } from 'react';
-
+import { useAppDispatch, useAppSelector } from '@store/store';
+import { useEffect, useCallback } from 'react';
+import { setCurrentPlayList } from '@store/features/trackSlice';
 import { handleAxiosError } from '@/utils/handleAxiosError';
 
 export default function Home() {
-  const [tracks, setTracks] = useState<Track[]>([]);
+  const dispatch = useAppDispatch();
+  const tracks = useAppSelector((state) => state.tracks.playList);
 
   const getAllTracks = useCallback(async () => {
     try {
       const data = await fetchAllTracks();
-      if (data) setTracks(data);
+      if (data) dispatch(setCurrentPlayList(data));
     } catch (error) {
       handleAxiosError(error);
     }
-  }, []);
+  }, [dispatch]);
 
   useEffect(() => {
     getAllTracks();
-  }, []);
+  }, [getAllTracks]);
 
   return (
     <>
